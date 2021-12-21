b514ae280097359a174f9aa6fa525c5424109e44
Kozachuk Kirill <kozachukkirill81@gmail.com>
2021-12-21 15:06:17 +0200

    е

--- Order.cpp
+++ Order.cpp
@@ -9,5 +9,5 @@ void Order(vector<string>& ValueKeeper)
     reverse(ValueKeeper.begin(), ValueKeeper.end());//1
 
     for (string i : ValueKeeper) cout << i << endl;// 2
-    cout << endl;// 2
+    cout << endl;//             2
 }
